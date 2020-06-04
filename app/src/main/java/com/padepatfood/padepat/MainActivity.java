d1998c48de5a562afa9215a4d0e2ec20d3a8bd0c
Tanguy <urbaintanguy@gmail.com>
2020-06-04 16:34:31 +0200

    Correction transition

--- app/src/main/java/com/padepatfood/padepat/MainActivity.java
+++ app/src/main/java/com/padepatfood/padepat/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.transition.Fade;
+import android.view.View;
 
 import java.util.Arrays;
 
@@ -24,6 +26,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         getSupportActionBar().hide();
+
+        Fade fade = new Fade();
+        View decor = getWindow().getDecorView();
+        fade.excludeTarget(decor.findViewById(R.id.action_bar_container), true);
+        fade.excludeTarget(android.R.id.statusBarBackground, true);
+        fade.excludeTarget(android.R.id.navigationBarBackground, true);
+
+        getWindow().setEnterTransition(fade);
+        getWindow().setExitTransition(fade);
 
         recipeList = getIntent().getParcelableArrayListExtra("recipeList");
 
