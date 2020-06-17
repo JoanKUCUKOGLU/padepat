a0d53bf9282384028ecee429671da06cde60555a
Melanie V <melanie.vaubien@hotmail.com>
2020-06-17 18:53:00 +0200

    Ajout d'un bouton de retour en arriere + gros Clean Code

--- app/src/main/java/com/padepatfood/padepat/LoginRegisterActivity.java
+++ app/src/main/java/com/padepatfood/padepat/LoginRegisterActivity.java
@@ -15,6 +15,13 @@ public class LoginRegisterActivity extends AppCompatActivity {
     Button registerButton;
     Button loginButton;
     LinearLayout loginRegisterLinearLayout;
+    Button goBackButton;
+    EditText pseudoInput;
+    EditText emailInput;
+    EditText passwordInput;
+    EditText passwordConfirmationInput;
+    Button saveButton;
+    boolean isStartPage = true;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -22,68 +29,137 @@ public class LoginRegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login_register);
         getSupportActionBar().hide();
 
+        findViews();
+        setPage();
+
+    }
+    //Regroupe tous les findViewById
+    private void findViews(){
         registerButton = findViewById(R.id.regiserButton);
         loginButton = findViewById(R.id.loginButton);
-
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.WRAP_CONTENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT
-        );
-        params.bottomMargin = 10;
-
         loginRegisterLinearLayout = findViewById(R.id.loginRegisterLinearLayout);
-        EditText pseudoInput = new EditText(LoginRegisterActivity.this);
-        pseudoInput.setEms(10);
-        pseudoInput.setLayoutParams(params);
-        pseudoInput.setHint("Pseudonyme");
+        goBackButton = findViewById(R.id.goBack);
+    }
 
-        EditText emailInput = new EditText(LoginRegisterActivity.this);
-        emailInput.setHint("Email");
-        emailInput.setEms(10);
-        emailInput.setLayoutParams(params);
+    //Remplis les view avec les datas correspondantes
+    private void setPage(){
 
-        EditText passwordInput = new EditText(LoginRegisterActivity.this);
-        passwordInput.setHint("Mot de passe");
-        passwordInput.setEms(10);
-        passwordInput.setLayoutParams(params);
-
-        EditText passwordConfirmationInput = new EditText(LoginRegisterActivity.this);
-        passwordConfirmationInput.setHint("Confirmation mot de passe");
-        passwordConfirmationInput.setEms(10);
-        passwordConfirmationInput.setLayoutParams(params);
-
-        Button saveButton = new Button(LoginRegisterActivity.this);
-        saveButton.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#FCE68B")));
-        saveButton.setTextColor(Color.parseColor("#000000"));
-        params.topMargin = 20;
-        params.bottomMargin = 0;
-        saveButton.setLayoutParams(params);
-
+        goBackButton.setVisibility(View.INVISIBLE);
+        generateItems();
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                loginRegisterLinearLayout.removeView(registerButton);
-                loginRegisterLinearLayout.removeView(loginButton);
-                loginRegisterLinearLayout.addView(pseudoInput);
-                loginRegisterLinearLayout.addView(emailInput);
-                loginRegisterLinearLayout.addView(passwordInput);
-                loginRegisterLinearLayout.addView(passwordConfirmationInput);
-                saveButton.setText("S'enregister");
-                loginRegisterLinearLayout.addView(saveButton);
+                setRegisterPage();
             }
         });
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                loginRegisterLinearLayout.removeView(registerButton);
-                loginRegisterLinearLayout.removeView(loginButton);
-                loginRegisterLinearLayout.addView(emailInput);
-                loginRegisterLinearLayout.addView(passwordInput);
-
-                saveButton.setText("Se connecter");
-                loginRegisterLinearLayout.addView(saveButton);
+                setLoginPage();
             }
         });
     }
+
+    //Créé des EditText ainsi qu'un bouton
+    private void generateItems(){
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        params.bottomMargin = 10;
+
+        pseudoInput = new EditText(LoginRegisterActivity.this);
+        setEditTextParams(pseudoInput,"Pseudonyme",params);
+
+        emailInput = new EditText(LoginRegisterActivity.this);
+        setEditTextParams(emailInput,"Email",params);
+
+        passwordInput = new EditText(LoginRegisterActivity.this);
+        setEditTextParams(passwordInput,"Mot de passe",params);
+
+        passwordConfirmationInput = new EditText(LoginRegisterActivity.this);
+        setEditTextParams(passwordConfirmationInput,"Confirmation mot de passe",params);
+
+        saveButton = new Button(LoginRegisterActivity.this);
+        saveButton.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#FCE68B")));
+        saveButton.setTextColor(Color.parseColor("#000000"));
+        params.topMargin = 20;
+        params.bottomMargin = 0;
+        saveButton.setLayoutParams(params);
+    }
+
+    //Affiche les éléments liés au login
+    private void setLoginPage(){
+        deletedChildrenFromIndex();
+        loginRegisterLinearLayout.addView(emailInput);
+        loginRegisterLinearLayout.addView(passwordInput);
+
+        saveButton.setText("Se connecter");
+        goBackButton.setText("Je n'ai pas de compte");
+        goBackButton.setVisibility(View.VISIBLE);
+        goBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setRegisterPage();
+            }
+        });
+        loginRegisterLinearLayout.addView(saveButton);
+        isStartPage = false;
+    }
+
+    //Affiche les éléments liés a la création d'un nouveau compte
+    private void setRegisterPage(){
+        deletedChildrenFromIndex();
+        loginRegisterLinearLayout.addView(pseudoInput);
+        loginRegisterLinearLayout.addView(emailInput);
+        loginRegisterLinearLayout.addView(passwordInput);
+        loginRegisterLinearLayout.addView(passwordConfirmationInput);
+        saveButton.setText("S'enregister");
+        goBackButton.setText("J'ai déjà un compte");
+        goBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setLoginPage();
+            }
+        });
+        goBackButton.setVisibility(View.VISIBLE);
+        loginRegisterLinearLayout.addView(saveButton);
+        isStartPage = false;
+    }
+
+    //Affiche les boutons de choix "Se connecter" et "S'enregistrer"
+    private void setStartPage(){
+        deletedChildrenFromIndex();
+        loginRegisterLinearLayout.addView(registerButton);
+        loginRegisterLinearLayout.addView(loginButton);
+        isStartPage = true;
+    }
+
+    // --- FONCTIONS OUTILS ---- //
+
+    //Fonction créée pour éviter la redondance de code - Applique les parametres layout, ainsi que le hint
+    private void setEditTextParams(EditText editText,String hintText, LinearLayout.LayoutParams params){
+        editText.setEms(10);
+        editText.setLayoutParams(params);
+        editText.setHint(hintText);
+    }
+    //Fonction créée pour éviter la redondance de code - supprime tous les éléments contenus dans le LinearLayout à l'exepté du 1er item
+    private void deletedChildrenFromIndex(){
+        int childNubmer = loginRegisterLinearLayout.getChildCount();
+        for(int i=1;i<childNubmer;i++){
+            loginRegisterLinearLayout.removeViewAt(1);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        //Ferme l'activity ou affiche à nouveau les boutons de choix "Se connecter" et "S'enregistrer"
+        if(isStartPage){
+            super.onBackPressed();
+        }else{
+            setStartPage();
+        }
+    }
+
 }
